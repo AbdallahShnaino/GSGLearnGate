1b6272d1cee669e51d98baf5cebde3e475426883
Mohammed Qashqesh <mohammedbelal601@gmail.com>
2025-03-19 16:29:47 +0200

    LEAR-13 create signup page without responsive

---
+++ components/SignupForm/SignupForm.tsx
@@ -0,0 +1,102 @@
+"use client";
+import React from "react";
+
+const SignupForm = () => {
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={submitForm}>
+      <div className="flex flex-col mt-4 gap-1.5">
+        <label htmlFor="email">Email*</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          className="border-1 border-gray-400 rounded-md px-1.5 py-1.5 focus:outline-blue-500"
+          required
+        />
+      </div>
+      <div className="flex flex-col mt-4 gap-1.5">
+        <label htmlFor="password">Password*</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          className="border-1 border-gray-400 rounded-md px-1.5 py-1.5 focus:outline-blue-500"
+          required
+        />
+        <p className="text-[14px] mt-1 text-[#777]">
+          Password should be at least 8 characters including a number. uppercase
+          and a lowercase Letter.
+        </p>
+      </div>
+      <div>
+        <div className="flex justify-between mt-4">
+          <div className="flex flex-col gap-1.5 w-[48%]">
+            <label htmlFor="firstName">First Name*</label>
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              placeholder="First Name"
+              className="border-1 border-gray-400 rounded-md px-1.5 py-1.5 focus:outline-blue-500"
+              required
+            />
+          </div>
+
+          <div className="flex flex-col gap-1.5 w-[48%]">
+            <label htmlFor="lastName">Last Name*</label>
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              placeholder="Last Name"
+              className="border-1 border-gray-400 rounded-md px-1.5 py-1.5 focus:outline-blue-500"
+              required
+            />
+          </div>
+        </div>
+        <p className="text-[14px] mt-1.5 text-[#777]">
+          First and last name may only contain letters and must not include
+          numbers or special characters.
+        </p>
+      </div>
+      <div className="flex justify-between mt-4">
+        <div className="flex flex-col gap-1.5 w-[48%]">
+          <label htmlFor="idNumber">Identification Number*</label>
+          <input
+            type="number"
+            name="idNumber"
+            id="idNumber"
+            placeholder="Identification Number"
+            className="border-1 border-gray-400 rounded-md px-1.5 py-1.5 focus:outline-blue-500"
+            required
+          />
+        </div>
+
+        <div className="flex flex-col gap-1.5 w-[48%]">
+          <label htmlFor="DOB">Date Of Birth*</label>
+          <input
+            type="date"
+            name="DOB"
+            id="DOB"
+            placeholder="Date Of Birth"
+            className="border-1 border-gray-400 rounded-md px-1.5 py-1.5 focus:outline-blue-500"
+            required
+          />
+        </div>
+      </div>
+      <input
+        type="submit"
+        value="Continue"
+        className="border-2 w-full rounded-md mt-4 p-2.5 text-lg cursor-pointer bg-[#1f2328] text-white hover:bg-[#32383f]"
+      />
+    </form>
+  );
+};
+
+export default SignupForm;
