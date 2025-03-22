cd33e862187567cd77f4445c1b0bbd5de670bbd6
Tasneem <tasneemgazal5@gmail.com>
2025-03-22 21:17:56 +0200

    LEAR-38 integrate users table in monitors and co-monitors pages and add route to add monitor page

--- components/UsersTables/UsersTable.tsx
+++ components/UsersTables/UsersTable.tsx
@@ -1,36 +1,51 @@
+"use client";
+import { useState } from "react";
 import { users } from "@/services/mock";
-import { TrashSimple, PencilSimple } from "@phosphor-icons/react/dist/ssr";
+import {
+  TrashSimple,
+  PencilSimple,
+  MagnifyingGlass,
+} from "@phosphor-icons/react/dist/ssr";
+import DeleteUserModal from "../DeleteUserModal/DeleteUserModal";
 
-export default function Table() {
+export default function UsersTable() {
+  const [open, setOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState<string>();
+
+  const handleDeleteClick = (user: string) => {
+    setSelectedUser(user);
+    setOpen(true);
+  };
+
+  const confirmDelete = () => {
+    if (selectedUser) {
+      console.log(`Deleting user with ID: ${selectedUser}`);
+    }
+    setOpen(false);
+    setSelectedUser("");
+  };
 
   return (
-    <div className="w-full max-w-4xl mx-auto p-4">
-      <div className="bg-[#222831] py-3 px-4 rounded-t-xl">
+    <div className="w-full max-w-4xl mx-auto p-4 space-y-4">
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <MagnifyingGlass size={20} className="text-[#FFA41F]" />
+        </div>
         <input
           type="text"
-          placeholder="Search users..."
-          className="p-2 border bg-white border-gray-300 rounded-lg"
+          className="block w-64 p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="Search for students"
         />
       </div>
-      <div className="overflow-hidden border border-gray-200 shadow-sm rounded-b-xl">
-        <table className="w-full border-collapse bg-white text-sm ">
-          <thead>
+      <div className="overflow-hidden border border-gray-200 shadow-sm rounded-xl">
+        <table className="w-full border-collapse bg-white text-sm">
+          <thead className="text-left text-xs text-gray-700 uppercase bg-gray-100">
             <tr className="bg-gray-50">
-              <th className="px-4 py-4 text-left font-medium text-gray-500">
-                ID
-              </th>
-              <th className="px-4 py-4 text-left font-medium text-gray-500">
-                Name
-              </th>
-              <th className="px-4 py-4 text-left font-medium text-gray-500">
-                Email
-              </th>
-              <th className="px-4 py-4 text-right font-medium text-gray-500">
-                Date of Birth
-              </th>
-              <th className="px-4 py-4 text-center font-medium text-gray-500">
-                Action
-              </th>
+              <th className="px-4 py-4 text-left">ID</th>
+              <th className="px-4 py-4 text-left">Name</th>
+              <th className="px-4 py-4 text-left">Email</th>
+              <th className="px-4 py-4 text-right">Date of Birth</th>
+              <th className="px-4 py-4 text-center">Action</th>
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-100">
@@ -45,9 +60,11 @@ export default function Table() {
                   {user.dob}
                 </td>
                 <td>
-                  <div className="flex justify-evenly ">
-                    <TrashSimple size={18} color="#ee1717" weight="fill" />
-                    <PencilSimple size={18} color="#179bee" weight="fill" />
+                  <div className="flex justify-evenly">
+                    <button onClick={() => handleDeleteClick(user.name)}>
+                      <TrashSimple size={18} color="#ee1717" weight="fill" />
+                    </button>
+                    <PencilSimple size={18} color="#1cc925" weight="fill" />
                   </div>
                 </td>
               </tr>
@@ -55,6 +72,13 @@ export default function Table() {
           </tbody>
         </table>
       </div>
+      {open && selectedUser && (
+        <DeleteUserModal
+          setOpen={setOpen}
+          confirmDelete={confirmDelete}
+          selectedUser={selectedUser}
+        />
+      )}
     </div>
   );
 }
