e796c42f3cb55e11bdd64e8c759e16bd6dc3a59c
AlaaMousa05 <loloadnan.2024@gmail.com>
2025-03-22 20:23:25 +0200

    LEAR-32 searchBar comp and use search hook

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,21 +1,22 @@
 import { MagnifyingGlass } from '@phosphor-icons/react/dist/ssr'
 import React from 'react'
 
-const SearchBar = () => {
+
+const SearchBar = ({ updateSearchParam }: { updateSearchParam: (newValue: string) => void }) => {
   return (
-<div className="flex items-center justify-between flex-wrap space-y-4 md:space-y-0 p-4 w-full mb-2 overflow-hidden">
-  <div className="relative">
-    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-    <MagnifyingGlass size={20} className='text-[#FFA41F]' />
+    <div className="flex items-center justify-between flex-wrap space-y-4 md:space-y-0 p-4 w-full mb-2 overflow-hidden">
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <MagnifyingGlass size={20} className='text-[#FFA41F]' />
+        </div>
+        <input
+          type="text"
+          className="block w-64 p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="Search for students"
+          onChange={(e) => updateSearchParam(e.target.value)}
+        />
+      </div>
     </div>
-    <input
-      type="text"
-      className="block w-64 p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-      placeholder="Search for students"/>
-  </div>
-</div>
-
-  )
-}
-
-export default SearchBar
+  );
+};
+export default SearchBar;
