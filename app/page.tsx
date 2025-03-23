72b3f109fc9d06010948aca2752dc1c2c7d42b55
Mohammed Qashqesh <mohammedbelal601@gmail.com>
2025-03-23 22:37:40 +0200

    LEAR-27 create footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import Link from "next/link";
-
+import CollectionCourses from "@/components/CollectionCourses/CollectionCourses";
+import Footer from "@/components/Footer/Footer";
+import HeaderNav from "@/components/HeaderNav/HeaderNav";
+import HeroSection from "@/components/HeroSection/HeroSection";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
-       <Link href="/CoMentor">CoMentor</Link>  
-</div>
+    <div>
+      <HeaderNav />
+      <HeroSection />
+      <CollectionCourses />
+      <Footer />
+    </div>
   );
 }
