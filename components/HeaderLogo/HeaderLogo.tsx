4462692b69495f95a91ddd23a95df17191086c28
Mohammed Qashqesh <mohammedbelal601@gmail.com>
2025-03-24 13:27:38 +0200

    LEAR-25 create about us page and few edit in headerNav

--- components/HeaderLogo/HeaderLogo.tsx
+++ components/HeaderLogo/HeaderLogo.tsx
@@ -1,10 +1,20 @@
 import Image from "next/image";
 
-const HeaderLogo = () => {
+interface positionProps {
+  position: string;
+}
+
+const HeaderLogo = ({ position }: positionProps) => {
   return (
     <div className="flex items-center gap-2.5 cursor-pointer">
       <Image src={"/img/logo.webp"} alt="GSG logo" width={50} height={50} />
-      <h1 className="text-xl font-bold lg:text-white">Gaza Sky Geeks</h1>
+      <h1
+        className={`text-xl font-bold  ${
+          position === "absolute" ? "lg:text-white" : "lg:text-black"
+        }`}
+      >
+        Gaza Sky Geeks
+      </h1>
     </div>
   );
 };
