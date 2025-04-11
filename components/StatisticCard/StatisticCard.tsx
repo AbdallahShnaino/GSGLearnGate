76331c322e87e127795081e53af9702d274f5c0f
Mohammed Qashqesh <mohammedbelal601@gmail.com>
2025-04-11 14:35:59 +0300

    LEAR-94 fix admin dashboard (dashboard page) without responsive chart

--- components/StatisticCard/StatisticCard.tsx
+++ components/StatisticCard/StatisticCard.tsx
@@ -8,8 +8,8 @@ const StatisticCard = (props:IProps) => {
   return (
     <div className="flex p-10 shadow-md shadow-gray-300/80 rounded-lg bg-[#f7fdff] justify-between items-center ">
         <div className="flex flex-col">
-            <p className="text-gray-400">{props.title}</p>
-            <p>{props.total}</p>
+            <p className="text-gray-400 text-xl">{props.title}</p>
+            <p className="text-lg">{props.total}</p>
         </div>
         <div className="bg-[#FFF5E8] p-2 rounded-2xl">
             {props.icon}
