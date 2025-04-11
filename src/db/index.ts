9fe76cb124e7f3a1fbdeb825700768cc694431b4
Mohammed Qashqesh <mohammedbelal601@gmail.com>
2025-04-11 16:02:58 +0300

    LEAR-94 back to original index code

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,34 +1,3 @@
-// import { config } from "dotenv";
-// import { drizzle } from "drizzle-orm/libsql";
-
-// config({ path: ".env" });
-
-// let db: any | undefined;
-
-// try {
-//   const connectionUrl = process.env.TURSO_CONNECTION_URL;
-//   const authToken = process.env.TURSO_AUTH_TOKEN;
-
-//   if (!connectionUrl || !authToken) {
-//     throw new Error("CODE:999");
-//   }
-
-//   db = drizzle({
-//     connection: {
-//       url: connectionUrl,
-//       authToken: authToken,
-//     },
-//   });
-// } catch (error: unknown) {
-//   if (error instanceof Error) {
-//     throw new Error("CODE:998");
-//   } else {
-//     throw new Error("CODE:997");
-//   }
-// }
-
-// export { db };
-
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/libsql";
 
@@ -37,11 +6,13 @@ config({ path: ".env" });
 let db: any | undefined;
 
 try {
-  const connectionUrl = "libsql://gsglearngatedb-abdallah-shnaino.turso.io";
+  const connectionUrl ="libsql://gsglearngatedb-abdallah-shnaino.turso.io";
   const authToken = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJleHAiOjE3NDY0MzUxMzcsImlhdCI6MTc0Mzg0MzEzNywiaWQiOiJhMDNmMzJiYi0wNWU4LTQzMjMtYTliZS0wODE3YTZlMjAwM2MifQ.CsQWpOXkMkRo35i5P6CkpXJ7ZqykxznPM5R4gJrhVvA4T0u8Kcj91vZw0WHTid38Ad1MYWiAWoL5wZjUsA5YAA";
 
   if (!connectionUrl || !authToken) {
-    throw new Error("CODE:999");
+    throw new Error(
+      "Missing required environment variables: TURSO_CONNECTION_URL or TURSO_AUTH_TOKEN"
+    );
   }
 
   db = drizzle({
@@ -52,9 +23,9 @@ try {
   });
 } catch (error: unknown) {
   if (error instanceof Error) {
-    throw new Error("CODE:998");
+    console.error("Error initializing database connection:", error.message);
   } else {
-    throw new Error("CODE:997");
+    console.error("An unknown error occurred during database initialization.");
   }
 }
 
